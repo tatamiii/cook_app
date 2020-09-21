8ad72d159816f4b33909dcaf675fcd91c0963a9a
tatamiii <sousuke2828@gmail.com>
2020-09-21 17:40:57 +0900

    ルーティングファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/terms'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
 end
